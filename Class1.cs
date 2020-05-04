20d1e7af4d1c1cdd2bebdcd6be15d6cca4bcbc86
houssemshili <48476266+houssemshili@users.noreply.github.com>
2020-05-04 13:31:48 +0200

    first commit

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDotNet
+{
+    public class Class1
+    {
+        public bool ReturnBool()
+        {
+            //comment
+            //comment
+            //comm
+            //comm
+            return true;
+        }
+    }
+}
---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ConsoleDotNet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 instance = new Class1();
+            Assert.IsTrue(instance.ReturnBool());
+        }
+    }
+}
